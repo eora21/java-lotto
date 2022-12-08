2b56d95273c46747d2194f808a2d7c7403c192f9
eora21 <eora21@naver.com>
2022-12-08 17:39:58 +0900

    feat: 🎸 추가 당첨 통계 출력 메서드

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,11 +1,16 @@
 package lotto.view;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
 import lotto.model.Lotto;
 import lotto.model.LottoNumber;
+import lotto.model.WinningRank;
 
 public class Output {
+
+    private static final NumberFormat numberFormat = NumberFormat.getInstance();
 
     public void requirePurchaseAmount() {
         notice("구입금액을 입력해 주세요.");
@@ -17,6 +22,26 @@ public class Output {
         for (Lotto lotto : lottoBundle) {
             noticeLottoNumbers(lotto);
         }
+    }
+
+    public void winningStatistics(Map<WinningRank, Integer> winningStatus) {
+        notice("당첨 통계");
+        notice("---");
+
+        winningStatus.forEach(((winningRank, count) -> {
+            notice(String.format("%s - %d개", createDescription(winningRank), count));
+        }));
+    }
+
+    private String createDescription(WinningRank winningRank) {
+        String description = winningRank.getDescription();
+
+        if (description.length() > 0) {
+            description = ", " + description;
+        }
+
+        return String.format("%d개 일치%s (%s원)",
+                winningRank.getCorrectCount(), description, numberFormat.format(winningRank.getPrizeMoney()));
     }
 
     private void noticeLottoNumbers(Lotto lotto) {
