25c6a3366d7bb57bb6392061c0dcbfe389312d21
eora21 <eora21@naver.com>
2022-12-08 16:17:23 +0900

    feat: 🎸 추가 사용자의 입력을 받아 로또 인스턴스를 생성하는 메서드

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,12 +1,24 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import lotto.model.Lotto;
 
 public class Input {
 
     public int purchaseAmount() {
         String input = Console.readLine();
         return toInt(input);
+    }
+
+    public Lotto winningNumber() {
+        String[] input = Console.readLine().split(",");
+
+        return new Lotto(
+                Arrays.stream(input)
+                        .map(this::toInt)
+                        .collect(Collectors.toList()));
     }
 
     private int toInt(String input) {
