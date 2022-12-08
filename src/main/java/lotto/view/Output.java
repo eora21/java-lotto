add8e122a8e70fc1a11ef413099a72e564414f54
eora21 <eora21@naver.com>
2022-12-08 16:01:21 +0900

    feat: 🎸 추가 로또 구매 갯수와 번호들 출력 메서드

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,9 +1,32 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.StringJoiner;
+import lotto.model.Lotto;
+import lotto.model.LottoNumber;
+
 public class Output {
 
     public void requirePurchaseAmount() {
         notice("구입금액을 입력해 주세요.");
+    }
+
+    public void lottoStatus(List<Lotto> lottoBundle) {
+        notice(String.format("%d개를 구매했습니다.", lottoBundle.size()));
+
+        for (Lotto lotto : lottoBundle) {
+            noticeLottoNumbers(lotto);
+        }
+    }
+
+    private void noticeLottoNumbers(Lotto lotto) {
+        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
+
+        for (LottoNumber number : lotto.getNumbers()) {
+            stringJoiner.add(String.valueOf(number.getLottoNumber()));
+        }
+
+        notice(stringJoiner.toString());
     }
 
     private void notice(String text) {
