c9df5a11a603e06aee831c80df51e9e87b0fc870
eora21 <eora21@naver.com>
2022-12-08 14:36:09 +0900

    feat: 🎸 추가 정적 팩토리 메서드 및 비교를 위한 메서드 재정의

--- src/main/java/lotto/model/LottoNumber.java
+++ src/main/java/lotto/model/LottoNumber.java
@@ -1,22 +1,51 @@
 package lotto.model;
 
-public class LottoNumber {
+public class LottoNumber implements Comparable<LottoNumber> {
 
     private static final int START = 1;
+
     private static final int END = 45;
 
     private final int lottoNumber;
 
-    public LottoNumber(int lottoNumber) {
-        validRange(lottoNumber);
+    private LottoNumber(int lottoNumber) {
         this.lottoNumber = lottoNumber;
     }
 
-    private void validRange(int lottoNumber) {
-        if (lottoNumber > START || END < lottoNumber) {
+    public static LottoNumber newInstance(int lottoNumber) {
+        validRange(lottoNumber);
+        return new LottoNumber(lottoNumber);
+    }
+
+    private static void validRange(int lottoNumber) {
+        if (lottoNumber < START || END < lottoNumber) {
             throw new IllegalArgumentException(String.format("로또 숫자의 범위는 %d부터 %d까지입니다.", START, END));
         }
     }
 
+    public int getLottoNumber() {
+        return lottoNumber;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return this.lottoNumber == that.lottoNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return lottoNumber;
+    }
+
+    @Override
+    public int compareTo(LottoNumber o) {
+        return this.lottoNumber - o.lottoNumber;
+    }
 }
