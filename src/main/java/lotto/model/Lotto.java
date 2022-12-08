ed8ea70755be3a06ca6fbdeeaedb898ba3e0df59
eora21 <eora21@naver.com>
2022-12-08 15:14:11 +0900

    feat: 🎸 추가 로또 객체 생성 시 범위 및 중복 검사, 정렬 후 생성하도록 작성

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,41 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
-    private final List<Integer> numbers;
+
+    private static final int SIZE = 6;
+
+    private final List<LottoNumber> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = createLottoNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validSize(numbers);
+        validDuplicate(numbers);
+    }
+
+    private static void validSize(List<Integer> numbers) {
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException(String.format("입력된 숫자들이 %d개로 이루어져있지 않습니다.", SIZE));
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validDuplicate(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != SIZE) {
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+        }
+    }
+
+    private List<LottoNumber> createLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .map(LottoNumber::newInstance)
+                .collect(Collectors.toList());
+    }
 }
